01236b56cd6a75f3d0ff30d4d9936fa5854ee74e
Chakri7reddy <chakridhar789@gmail.com>
2023-04-05 09:59:57 +0530

    This is Java code

---
+++ Chakrijava.java
@@ -0,0 +1,13 @@
+
+public class Chakrijava {
+
+	public static void main(String[] args) {
+		System.out.println("chakri");
+		int a,b,c;
+		a=10;b=20;
+		c=a+b;
+		System.out.println("add="+c);
+
+	}
+
+}
---
+++ Constuctor.java
@@ -0,0 +1,19 @@
+class Testconstructor
+{
+	public Testconstructor() {
+		System.out.println();
+	}
+	public Testconstructor(int a,int b)
+	{
+		int c=a-b;
+		System.out.println(c);
+	}
+}
+public class Constuctor {
+
+	public static void main(String[] args) {
+	new Testconstructor(16,4);
+
+	}
+
+}
